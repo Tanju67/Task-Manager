b0735b4cdc02d4a19b793c9ae5a3df3f0d5f5ecb
Tanju Özer <tanjuozer64@gmail.com>
2023-12-24 20:11:12 +0100

    setup react-router-dom

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,20 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
+import TaskManagerPage from "./pages/TaskManagerPage";
+
 function App() {
-  return <div>hello</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/tasks-manager" element={<TaskManagerPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
---
+++ frontend/src/pages/TaskManagerPage.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function TaskManagerPage() {
+  return <div>TaskManagerPage</div>;
+}
+
+export default TaskManagerPage;
