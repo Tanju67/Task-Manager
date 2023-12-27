2061108c3a7c9cfd48c8ace50344bf8c2cea7478
Tanju Özer <tanjuozer64@gmail.com>
2023-12-27 19:50:49 +0100

    setup basic auth routes and controllers

--- backend/app.js
+++ backend/app.js
@@ -2,7 +2,18 @@ require("express-async-errors");
 require("dotenv").config();
 const express = require("express");
 const connectDB = require("./db/connect");
+const errorHandler = require("./errors/error-handler");
+const notFound = require("./errors/not-found");
+const authRoutes = require("./routes/authRoutes");
 const app = express();
+
+app.use(express.json());
+
+app.use("api/v1/auth", authRoutes);
+
+//error handler and not found middleware
+app.use(notFound);
+app.use(errorHandler);
 
 const port = process.env.PORT || 8000;
 const start = async () => {
