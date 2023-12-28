16e7324a2f8cf499b8633e49aaf3213ad0e6c2b8
Tanju Özer <tanjuozer64@gmail.com>
2023-12-28 21:38:08 +0100

    build category controllers middleware

--- backend/controllers/taskControllers.js
+++ backend/controllers/taskControllers.js
@@ -1,8 +1,26 @@
-const createCategory = async (req, res, next) => {};
+const CategoryModel = require("../models/categoryModel");
+const TaskModel = require("../models/taskModel");
+const { createCustomError } = require("../errors/customError");
 
-const getAllCategories = async (req, res, next) => {};
+const createCategory = async (req, res, next) => {
+  const { category } = req.body;
+  await CategoryModel.create({ category, creator: req.userData.userId });
+  res.status(201).json({ category });
+};
 
-const deleteCategory = async (req, res, next) => {};
+const getAllCategories = async (req, res, next) => {
+  const category = await CategoryModel.find({ creator: req.userData.userId });
+  res.status(200).json({ category });
+};
+
+const deleteCategory = async (req, res, next) => {
+  const id = req.params.cId;
+  const category = await CategoryModel.findByIdAndDelete(id);
+  if (!category) {
+    return next(createCustomError("Found no category for provided id.", 404));
+  }
+  res.status(200).json({ category });
+};
 
 const createTask = async (req, res, next) => {};
 
