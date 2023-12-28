46d356c6d07f2f753094ec6d302b5451004580b0
Tanju Özer <tanjuozer64@gmail.com>
2023-12-28 19:11:29 +0100

    initial setup for task controllers

---
+++ backend/controllers/taskControllers.js
@@ -0,0 +1,23 @@
+const createCategory = async (req, res, next) => {};
+
+const getAllCategories = async (req, res, next) => {};
+
+const deleteCategory = async (req, res, next) => {};
+
+const createTask = async (req, res, next) => {};
+
+const getAllTask = async (req, res, next) => {};
+
+const updateTask = async (req, res, next) => {};
+
+const deleteTask = async (req, res, next) => {};
+
+module.exports = {
+  createCategory,
+  getAllCategories,
+  deleteCategory,
+  createTask,
+  getAllTask,
+  updateTask,
+  deleteTask,
+};
